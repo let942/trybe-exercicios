bb2ac60ea9302913d836b9234fad777cb9a36500
Letícia Chagas Marques <let942@gmail.com>
2022-02-21 22:58:28 -0300

    feat: adiciona bônus 2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,6 +1,24 @@
 const enviar = document.getElementById('enviar');
+const nome = document.getElementById('input-nome-completo');
+const email = document.getElementById('input-email');
+const textoArea = document.getElementById('texto-area');
 
 // enterrompe botão submit
 // enviar.addEventListener('click', function(event){
 //   event.preventDefault()
 // });
+
+function validacao(event) {
+  event.preventDefault()
+
+  if(email.value.length < 10 || email.value.length > 50|| nome.value.length < 10 || nome.value.length > 40 || textoArea.value.length > 500){
+    alert('Dados Inválidos');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
+
+}
+
+
+enviar.addEventListener('click', validacao);
+
